2529dc8add8e3f982915f52695f28f4628a2797a
Dongruixuan Li <dongruixuan@hotmail.com>
2023-06-29 19:36:39 +0800

    feat: make warp optional

--- worker/run.sh
+++ worker/run.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
-/bin/warp-svc &
-sleep 5
-/bin/warp-cli --accept-tos register
-/bin/warp-cli --accept-tos set-mode proxy
-/bin/warp-cli --accept-tos set-proxy-port 7777
-/bin/warp-cli --accept-tos connect
+if [ "$WARP_ENABLED" = "true" ]; then
+    /bin/warp-svc &
+    sleep 5
+    /bin/warp-cli --accept-tos register
+    /bin/warp-cli --accept-tos set-mode proxy
+    /bin/warp-cli --accept-tos set-proxy-port 7777
+    /bin/warp-cli --accept-tos connect
+fi
 /app/worker
