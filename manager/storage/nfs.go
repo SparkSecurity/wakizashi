a4c1f5cd17742ea0587f6cd63776bd99a7b7c1a2
Dongruixuan Li <dongruixuan@hotmail.com>
2023-07-03 15:58:23 +0800

    fix: trying to fix the nfs "rpc: client is shutting down" problem

--- manager/storage/nfs.go
+++ manager/storage/nfs.go
@@ -6,14 +6,33 @@ import (
 	"github.com/sile16/go-nfs-client/nfs/rpc"
 	"io"
 	"log"
+	"strings"
+	"sync"
 )
 
+type connectionInfo struct {
+	server      string
+	uid, gid    uint32
+	machineName string
+	basePath    string
+}
+
 type NFSStorage struct {
-	client *nfs.Target
+	connectionInfo connectionInfo
+	client         *nfs.Target
 	StorageInterface
 }
 
+var reconnectLock sync.Mutex
+
 func (s *NFSStorage) Init(server string, uid, gid uint32, machineName string, basePath string) (err error) {
+	s.connectionInfo = connectionInfo{
+		server:      server,
+		uid:         uid,
+		gid:         gid,
+		machineName: machineName,
+		basePath:    basePath,
+	}
 	mount, err := nfs.DialMount(server, false)
 	if err != nil {
 		log.Fatalf("unable to dial MOUNT service: %v", err)
@@ -28,12 +47,44 @@ func (s *NFSStorage) Init(server string, uid, gid uint32, machineName string, ba
 	return
 }
 
+func (s *NFSStorage) reInit() (err error) {
+	mount, err := nfs.DialMount(s.connectionInfo.server, false)
+	if err != nil {
+		log.Fatalf("unable to dial MOUNT service: %v", err)
+	}
+	auth := rpc.NewAuthUnix(s.connectionInfo.machineName, s.connectionInfo.uid, s.connectionInfo.gid)
+
+	v, err := mount.Mount(s.connectionInfo.basePath, auth.Auth(), false)
+	if err != nil {
+		log.Fatalf("unable to mount volume: %v", err)
+	}
+	s.client = v
+	return
+}
+
+func (s *NFSStorage) EnsureConnected() {
+	reconnectLock.Lock()
+	defer reconnectLock.Unlock()
+	if _, err := s.client.FSInfo(); err != nil {
+		if strings.Contains(err.Error(), "rpc: client is shutting down") {
+			log.Println("NFS client is shutting down, reconnecting...")
+			err := s.reInit()
+			if err != nil {
+				panic(err)
+			}
+		} else {
+			panic(err)
+		}
+	}
+}
+
 type readCloser struct {
 	io.Reader
 	io.Closer
 }
 
 func (s *NFSStorage) DownloadFile(fileId string) (stream io.ReadCloser, err error) {
+	s.EnsureConnected()
 	f, err := s.client.Open(fileId)
 	if err != nil {
 		return nil, err
--- worker/storage/nfs.go
+++ worker/storage/nfs.go
@@ -7,14 +7,33 @@ import (
 	"github.com/sile16/go-nfs-client/nfs/rpc"
 	"io"
 	"log"
+	"strings"
+	"sync"
 )
 
+type connectionInfo struct {
+	server      string
+	uid, gid    uint32
+	machineName string
+	basePath    string
+}
+
 type NFSStorage struct {
-	client *nfs.Target
+	connectionInfo connectionInfo
+	client         *nfs.Target
 	StorageInterface
 }
 
+var reconnectLock sync.Mutex
+
 func (s *NFSStorage) Init(server string, uid, gid uint32, machineName string, basePath string) (err error) {
+	s.connectionInfo = connectionInfo{
+		server:      server,
+		uid:         uid,
+		gid:         gid,
+		machineName: machineName,
+		basePath:    basePath,
+	}
 	mount, err := nfs.DialMount(server, false)
 	if err != nil {
 		log.Fatalf("unable to dial MOUNT service: %v", err)
@@ -29,7 +48,39 @@ func (s *NFSStorage) Init(server string, uid, gid uint32, machineName string, ba
 	return
 }
 
+func (s *NFSStorage) reInit() (err error) {
+	mount, err := nfs.DialMount(s.connectionInfo.server, false)
+	if err != nil {
+		log.Fatalf("unable to dial MOUNT service: %v", err)
+	}
+	auth := rpc.NewAuthUnix(s.connectionInfo.machineName, s.connectionInfo.uid, s.connectionInfo.gid)
+
+	v, err := mount.Mount(s.connectionInfo.basePath, auth.Auth(), false)
+	if err != nil {
+		log.Fatalf("unable to mount volume: %v", err)
+	}
+	s.client = v
+	return
+}
+
+func (s *NFSStorage) EnsureConnected() {
+	reconnectLock.Lock()
+	defer reconnectLock.Unlock()
+	if _, err := s.client.FSInfo(); err != nil {
+		if strings.Contains(err.Error(), "rpc: client is shutting down") {
+			log.Println("NFS client is shutting down, reconnecting...")
+			err := s.reInit()
+			if err != nil {
+				panic(err)
+			}
+		} else {
+			panic(err)
+		}
+	}
+}
+
 func (s *NFSStorage) UploadFile(stream io.Reader) (fileId string, err error) {
+	s.EnsureConnected()
 	s256 := sha256.New()
 	// potential memory overuse
 	rawBytes, err := io.ReadAll(stream)
