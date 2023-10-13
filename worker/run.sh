dc359cffdf268c83ac9f8cee682ae094aba3d6a3
Atum <atum@Atums-MacBook-Pro.local>
2023-10-13 15:29:48 +0800

    add a clash client for proxy

--- worker/run.sh
+++ worker/run.sh
@@ -1,10 +1,17 @@
 #!/bin/bash
-if [ "$WARP_ENABLED" = "true" ]; then
+if [ "$PROXY_MODE" = "warp" ]; then
     /bin/warp-svc &
     sleep 5
     /bin/warp-cli --accept-tos register
     /bin/warp-cli --accept-tos set-mode proxy
     /bin/warp-cli --accept-tos set-proxy-port 7777
     /bin/warp-cli --accept-tos connect
+    PROXY=socks5://127.0.0.1:7777 /app/worker
+elif [ "$PROXY_MODE" = "vmess" ]; then
+    sed -i "s/vmess_server_ip/$VMESS_SERVER_IP/g" /app/clash/config.yaml
+    sed -i "s/vmess_uuid/$VMESS_UUID/g" /app/clash/config.yaml
+    pm2 start clash -d /app/clash
+    PROXY=socks5://127.0.0.1:7890 /app/worker
+else
+    /app/worker
 fi
-/app/worker
